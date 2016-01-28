d64d0ce999c807a6d9f8dccad69521fbbda509a1
Spencer Harris <sharris40@uco.edu>
2016-01-27 22:35:34 -0600

    Removed temporary comments and changed name.

--- wsp1SpencerH/src/java/edu/uco/sharris40/wsp1spencerh/LoginServlet.java
+++ wsp1SpencerH/src/java/edu/uco/sharris40/wsp1spencerh/LoginServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.uco.sharris40.wsp1spencerh;
 
 import java.io.IOException;
@@ -15,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
- * @author dpbjinc
+ * Handles login for the site.
+ * @author Spencer Harris
  */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
