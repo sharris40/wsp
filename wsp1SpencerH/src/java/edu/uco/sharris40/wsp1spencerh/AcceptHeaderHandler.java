376d4ba03665dd298c45b01ef599cffc87f070d5
Spencer Harris <sharris40@uco.edu>
2016-01-27 21:16:06 -0600

    Test branch for Accept header processing.

---
+++ wsp1SpencerH/src/java/edu/uco/sharris40/wsp1spencerh/AcceptHeaderHandler.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uco.sharris40.wsp1spencerh;
+
+/**
+ *
+ * @author dpbjinc
+ */
+public class AcceptHeaderHandler {
+  private enum MediaTypeCombination {
+    ANY,
+    ANY_ASCII,
+    ANY_OTHER_CHARSET,
+    ANY_PARAM,
+    ANY_PARAM_ASCII,
+    ANY_PARAM_OTHER_CHARSET,
+    APPLICATION,
+    APPLICATION_ASCII,
+    APPLICATION_OTHER_CHARSET,
+    APPLICATION_PARAM,
+    APPLICATION_PARAM_ASCII,
+    APPLICATION_PARAM_OTHER_CHARSET,
+    TEXT,
+    TEXT_ASCII,
+    TEXT_OTHER_CHARSET,
+    TEXT_PARAM,
+    TEXT_PARAM_ASCII,
+    TEXT_PARAM_OTHER_CHARSET,
+    XHTML,
+    XHTML_ASCII,
+    XHTML_OTHER_CHARSET,
+    XHTML_PARAM,
+    XHTML_PARAM_ASCII,
+    XHTML_PARAM_OTHER_CHARSET,
+    HTML,
+    HTML_ASCII,
+    HTML_OTHER_CHARSET,
+    HTML_PARAM,
+    HTML_PARAM_ASCII,
+    HTML_PARAM_OTHER_CHARSET
+  }
+}
--- wsp1SpencerH/src/java/edu/uco/sharris40/wsp1spencerh/LoginServlet.java
+++ wsp1SpencerH/src/java/edu/uco/sharris40/wsp1spencerh/LoginServlet.java
@@ -7,11 +7,18 @@ package edu.uco.sharris40.wsp1spencerh;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.glassfish.jersey.message.internal.HttpHeaderReader;
+import org.glassfish.jersey.message.internal.AcceptableMediaType;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Map;
 
 /**
  *
@@ -19,6 +26,78 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
+  
+  private String toLowerCaseASCII(String input) {
+    char[] chars = input.toCharArray();
+    for (int i = 0; i < chars.length; ++i) {
+      if (chars[i] >= 'A' && chars[i] <= 'Z') {
+        chars[i] += 0x20;
+      }
+    }
+    return new String(chars);
+  }
+
+  private String remapCharset(String charset) {
+    switch (toLowerCaseASCII(charset)) {
+      case "iso-ir-6":
+      case "ansi_x3.4-1968":
+      case "ansi_x3.4-1986":
+      case "iso_646.irv:1991":
+      case "iso646-us":
+      case "us-ascii":
+      case "us":
+      case "ibm367":
+      case "cp367":
+      case "csascii":
+        return "us-ascii";
+      case "utf-8":
+      case "csutf8":
+        return "utf-8";
+      default:
+        return "";
+    }
+  }
+  
+  private boolean useXML(String header) {
+    List<AcceptableMediaType> types = null;
+    try {
+      types = HttpHeaderReader.readAcceptMediaType(header);
+    } catch (ParseException ex) {
+      Logger.getLogger(LoginServlet.class.getName()).log(Level.WARNING, "Received invalid Accept header from client; using default media type.", ex);
+      return true;
+    }
+    for (AcceptableMediaType type : types) {
+      String mainType = type.getType();
+      String subType = type.getSubtype();
+      int qValue = type.getQuality();
+      Map<String, String> params = type.getParameters();
+      boolean hasExtraParam = false;
+      String charset = null;
+      for (String key : params.keySet()) {
+        if (toLowerCaseASCII(key).equals("charset")) {
+          charset = remapCharset(params.get(key));
+        } else {
+          hasExtraParam = true;
+        }
+      }
+      switch (mainType) {
+        case "*":
+          if (subType.equals("*")) {
+            if (!hasExtraParam && charset != "") {
+              return type;
+            } else if 
+          }
+          break;
+        case "application":
+          break;
+        case "text":
+          break;
+        default:
+          break;
+      }
+    }
+    return backupType;
+  }
 
   /**
    * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
