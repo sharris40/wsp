7e8496f4e2b065a30f11ed8fab6adb69eae2bc6c
Spencer Harris <sharris40@uco.edu>
2016-03-23 22:26:43 -0500

    Fixed database scripts.

--- wsp7SpencerH/src/java/orderDB.sql
+++ wsp7SpencerH/src/java/orderDB.sql
@@ -1,19 +1,12 @@
 -- user wsp, pw wsp1SpencerH!
 USE `wsp`;
-DROP table IF EXISTS "books";
+DROP table IF EXISTS "orders";
 
-CREATE TABLE "books" (
-  "bookid" int(11) NOT NULL AUTO_INCREMENT,
-  "title" nvarchar(32) NOT NULL,
-  "author" nvarchar(32) NOT NULL,
-  "price" int(6) unsigned DEFAULT NULL,
-  "publicationYear" int(5) unsigned NOT NULL,
-  PRIMARY KEY ("bookid")
+CREATE TABLE "orders" (
+  "orderid" int(11) NOT NULL,
+  "bookid" int(11) NOT NULL,
+  "quantity" int(11) unsigned NOT NULL,
+  "time" timestamp NOT NULL DEFAULT NOW(),
+  PRIMARY KEY ("orderid", "bookid"),
+  FOREIGN KEY ("bookid") REFERENCES "books"("bookid")
 );
-
-INSERT INTO books (title, author, price, publicationYear)
-    VALUES ('A Good Book', 'Great Author', 20000, 2002);
-INSERT INTO books (title, author, price, publicationYear)
-    VALUES ('A Mediocre Book', 'Okay Author', 3500, 2013);
-INSERT INTO books (title, author, price, publicationYear)
-    VALUES ('A Bad Book', 'Terrible Author', 500, 2005);
